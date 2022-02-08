fa815a9e0af80e329cfbd211bd17ee564cbd964d
Oskari Arponen <oskarar@metropolia.fi>
2022-02-09 01:51:50 +0200

    add home top tab navigation

--- navigators/Navigator.js
+++ navigators/Navigator.js
@@ -3,6 +3,7 @@ import CustomNavBar from "./CustomNavBar";
 import {NavigationContainer} from "@react-navigation/native";
 import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
 import {createNativeStackNavigator} from "@react-navigation/native-stack";
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import Home from "../views/Home";
 import Login from "../views/Login";
 import Profile from "../views/Profile";
@@ -11,17 +12,17 @@ import Post from "../views/Post";
 import Notifications from "../views/Notifications";
 import Settings from "../views/Settings";
 import {MainContext} from "../contexts/MainContext";
-
 import colors from "../global/colors.json";
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
-const StackScreen = () => {
+const HomeTopTab = createMaterialTopTabNavigator();
 
+const getColors = () => {
   const {darkMode} = useContext(MainContext);
 
-  let bgColor, headerColor;
+  let bgColor, headerColor, headerTintColor, highlightColor = colors.highlight_color;
 
   if (darkMode) {
     bgColor = colors.dark_mode_bg;
@@ -32,6 +33,31 @@ const StackScreen = () => {
     headerColor = colors.light_mode_header;
     headerTintColor = colors.light_mode_header_tint;
   }
+  return {bgColor, headerColor, headerTintColor, highlightColor};
+}
+
+
+const HomeTopNavigator = () => {
+
+  const colors = getColors();
+
+  return (
+    <HomeTopTab.Navigator screenOptions={{
+      tabBarStyle: {backgroundColor: colors.headerColor},
+      tabBarLabelStyle: {color: colors.headerTintColor},
+      tabBarIndicatorStyle: {backgroundColor: colors.highlightColor},
+    }}>
+      <HomeTopTab.Screen name="Recent" component={Home} />
+      <HomeTopTab.Screen name="Popular" component={Home} />
+    </HomeTopTab.Navigator>
+  )
+}
+
+
+
+const StackScreen = () => {
+
+  const colors = getColors();
 
   return (
     <Stack.Navigator>
@@ -40,43 +66,31 @@ const StackScreen = () => {
       }} />}
       <Stack.Screen name="Login" component={Login} options={{
         headerStyle: {
-          backgroundColor: headerColor,
+          backgroundColor: colors.headerColor,
         },
-        headerTintColor: 'white',
+        headerTintColor: colors.headerTintColor,
       }} />
-      {/*       <Stack.Screen name="Register" />
-      <Stack.Screen name="Welcome" /> */}
     </Stack.Navigator>
   );
 };
 
 const BottomNav = () => {
   const {isLoggedIn} = useContext(MainContext);
-  const {darkMode} = useContext(MainContext);
 
-  let bgColor, headerColor;
+  const colors = getColors();
 
-  if (darkMode) {
-    bgColor = colors.dark_mode_bg;
-    headerColor = colors.dark_mode_header;
-    headerTintColor = colors.dark_mode_header_tint;
-  } else {
-    bgColor = colors.light_mode_bg;
-    headerColor = colors.light_mode_header;
-    headerTintColor = colors.light_mode_header_tint;
-  }
   return (
     <Tab.Navigator
       initialRouteName="Home"
       tabBar={(props) => <CustomNavBar {...props} />}
     >
       <Tab.Screen name="Home"
-        component={Home}
+        component={HomeTopNavigator}
         options={{
           headerStyle: {
-            backgroundColor: headerColor
+            backgroundColor: colors.headerColor,
           },
-          headerTintColor: headerTintColor
+          headerTintColor: colors.headerTintColor
         }}
       />
       {isLoggedIn && <Tab.Screen
@@ -84,45 +98,45 @@ const BottomNav = () => {
         component={Profile}
         options={{
           headerStyle: {
-            backgroundColor: headerColor
+            backgroundColor: colors.headerColor
           },
-          headerTintColor: headerTintColor
+          headerTintColor: colors.headerTintColor
         }} />}
       {isLoggedIn && <Tab.Screen
         name="CreateView"
         component={Create}
         options={{
           headerStyle: {
-            backgroundColor: headerColor
+            backgroundColor: colors.headerColor
           },
-          headerTintColor: headerTintColor
+          headerTintColor: colors.headerTintColor
         }} />}
       <Tab.Screen
         name="Notifications"
         component={Notifications}
         options={{
           headerStyle: {
-            backgroundColor: headerColor
+            backgroundColor: colors.headerColor
           },
-          headerTintColor: headerTintColor
+          headerTintColor: colors.headerTintColor
         }} />
       <Tab.Screen
         name="Settings"
         component={Settings}
         options={{
           headerStyle: {
-            backgroundColor: headerColor
+            backgroundColor: colors.headerColor
           },
-          headerTintColor: headerTintColor
+          headerTintColor: colors.headerTintColor
         }} />
       <Tab.Screen
         name="Post"
         component={Post}
         options={{
           headerStyle: {
-            backgroundColor: headerColor
+            backgroundColor: colors.headerColor
           },
-          headerTintColor: headerTintColor
+          headerTintColor: colors.headerTintColor
         }} />
     </Tab.Navigator>
   );
