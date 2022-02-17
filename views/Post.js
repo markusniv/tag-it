edfde3b19d579d5225d40c8b17129967c68d4beb
Markus Nivasalo <markus.nivasalo@outlook.com>
2022-02-17 13:31:28 +0200

    Started working on post view

--- views/Post.js
+++ views/Post.js
@@ -1,17 +1,17 @@
-import React, { useState, useEffect } from "react";
-import { StyleSheet, SafeAreaView, Text, View } from "react-native";
-import { Image } from "react-native-elements";
+import React, {useState, useEffect} from "react";
+import {StyleSheet, SafeAreaView, Text, View, ScrollView, } from "react-native";
+import {Image} from "react-native-elements";
 import PropTypes from "prop-types";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
-import { Video } from "expo-av";
-import { useUser } from "../hooks/ApiHooks";
+import {Video} from "expo-av";
+import {useUser} from "../hooks/ApiHooks";
 
-const Post = ({ route }) => {
-  const { getUserById } = useUser();
+const Post = ({route}) => {
+  const {getUserById} = useUser();
   const video = React.useRef(null);
   const [status, setStatus] = useState({});
   const url = "https://media.mw.metropolia.fi/wbma/uploads/";
-  const { media } = route.params;
+  const {media} = route.params;
   const singleMedia = media.singleMedia;
   const [user, setUser] = useState(singleMedia.user_id);
   const fileType = singleMedia.mime_type.split("/").shift();
@@ -27,36 +27,38 @@ const Post = ({ route }) => {
 
   return (
     <SafeAreaView style={styles.container}>
-      {fileType === "image" ? (
-        <Image
-          containerStyle={{ aspectRatio: 1, width: "90%", alignSelf: "center" }}
-          source={{ uri: `${url}${singleMedia.filename}` }}
-          resizeMode="contain"
-        />
-      ) : (
-        <Video
-          ref={video}
-          style={{ aspectRatio: 1, width: "90%", alignSelf: "center" }}
-          source={{ uri: `${url}${singleMedia.filename}` }}
-          useNativeControls
-          resizeMode="contain"
-          isLooping
-          onPlaybackStatusUpdate={(status) => setStatus(() => status)}
-        />
-      )}
-      <View style={styles.bottom}>
-        <MaterialCommunityIcons
-          style={{ alignSelf: "center", padding: 10 }}
-          name="image"
-          color="black"
-          size={30}
-        />
-        <View style={styles.bottomText}>
-          <Text style={{ fontSize: 20 }}>{singleMedia.title}</Text>
-          <Text style={{ fontSize: 18 }}>{singleMedia.description}</Text>
-          <Text style={{ fontSize: 20 }}>By {user}</Text>
+      <ScrollView>
+        {fileType === "image" ? (
+          <Image
+            containerStyle={{aspectRatio: 1, width: "90%", alignSelf: "center"}}
+            source={{uri: `${url}${singleMedia.filename}`}}
+            resizeMode="contain"
+          />
+        ) : (
+          <Video
+            ref={video}
+            style={{aspectRatio: 1, width: "90%", alignSelf: "center"}}
+            source={{uri: `${url}${singleMedia.filename}`}}
+            useNativeControls
+            resizeMode="contain"
+            isLooping
+            onPlaybackStatusUpdate={(status) => setStatus(() => status)}
+          />
+        )}
+        <View style={styles.bottom}>
+          <MaterialCommunityIcons
+            style={{alignSelf: "center", padding: 10}}
+            name="image"
+            color="black"
+            size={30}
+          />
+          <View style={styles.bottomText}>
+            <Text style={{fontSize: 20}}>{singleMedia.title}</Text>
+            <Text style={{fontSize: 18}}>{singleMedia.description}</Text>
+            <Text style={{fontSize: 20}}>By {user}</Text>
+          </View>
         </View>
-      </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
